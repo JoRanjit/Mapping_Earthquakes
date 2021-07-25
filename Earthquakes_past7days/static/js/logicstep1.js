4772e7d28fe667e335ebdae649fe281c33faee5e
JoRanjit <83322489+JoRanjit@users.noreply.github.com>
2021-07-24 18:13:01 -0700

    Earthquakes_past7days

---
+++ Earthquakes_past7days/static/js/logicstep1.js
@@ -0,0 +1,58 @@
+// We create the satelliet strrets tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//streets.addTo(map);
+//create a base layer that holds both maps
+let baseMaps = {
+  Street: streets,
+  Satellite: satelliteStreets
+};
+
+//create a map object with center and zoom
+let map = L.map("mapid",{
+  center:[39.5,-98.5], //center of United States
+  zoom:3,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//Accessing major airports geoJSON via github raw url
+let earthQuakeSummary = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+//Create a style for lines
+let myStyle = {
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
+};
+//Grabbing our GeoJSON data from URL using d3.JSON
+d3.json(earthQuakeSummary).then(function(data){
+ 
+  //Creating a GeoJSPN layer with the retreived data
+  L.geoJSON(data,{
+     style: myStyle,
+    //  onEachFeature: function(feature,layer){
+    //   layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>"); 
+    //}
+  }).addTo(map);
+});
+
+
+
+
+
+
+
