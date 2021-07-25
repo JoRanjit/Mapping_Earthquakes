07dc3a53a26d99fb07ee094c85408e30bced03e2
JoRanjit <83322489+JoRanjit@users.noreply.github.com>
2021-07-25 10:50:26 -0700

    Earthquake_Challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -150,7 +150,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
       return "#ea2c2c"; }
     else if (magnitude>5) {
       return "#ea822c";  }
-    else{
+    else {
       return "#98ee00"   }    
   };
   
@@ -169,7 +169,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
     pointToLayer: function(feature,latlng) {
       return L.circleMarker(latlng);
     },
-    style: styleInfo,    
+    style: styleInfo,
+    onEachFeature: function(feature,layer) {
+      layer.bindPopup("<h3> Major Earthquake </h3> <hr> <h4> Magnitude: "+ feature.properties.mag +"<br> Location: " + feature.properties.place + "</h4>");
+  }
+
   }).addTo(majorEQ);
   // 8. Add the major earthquakes layer to the map.
   majorEQ.addTo(map);
